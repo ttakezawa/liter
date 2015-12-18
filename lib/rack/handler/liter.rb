87aabf62841ec62c55f695634cf7522cd62ffdd9
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2015-12-18 11:18:02 +0900

    Refactor request parser with Buffered IO Interface

--- lib/rack/handler/liter.rb
+++ lib/rack/handler/liter.rb
@@ -16,26 +16,17 @@ module Rack
 
       def run(app, options)
         Socket.tcp_server_loop(options[:Host], options[:Port]) do |socket, addr|
-          buf = ''
-          while true
-            buf << socket.sysread(4096)
-            break if buf[-4,4] == "\r\n\r\n"
-          end
-
-          reqs = buf.split("\r\n")
-
-          req = reqs.shift.split
-          path_info, query_string = req[1].split('?')
+          verb, uri, http_version = socket.gets.split(' ')
+          path_info, query_string = uri.split('?')
 
           env = {
-            'REQUEST_METHOD'    => req[0],
+            'REQUEST_METHOD'    => verb,
             'SCRIPT_NAME'       => '',
             'PATH_INFO'         => path_info,
             'QUERY_STRING'      => query_string || '',
             'SERVER_NAME'       => options[:Host],
             'SERVER_PORT'       => options[:Port],
             'REMOTE_ADDR'       => addr.ip_address,
-
             'rack.version'      => [1, 3],
             'rack.url_scheme'   => 'http',
             'rack.input'        => StringIO.new('').set_encoding('BINARY'),
@@ -44,11 +35,14 @@ module Rack
             'rack.multiprocess' => false,
             'rack.run_once'     => false,
           }
-          reqs.each do |header|
-            header = header.split(': ')
-            env['HTTP_' + header[0].upcase.tr('-', '_')] = header[1];
+
+          while (header = socket.gets).start_with?("\r\n")
+            k, v = header.split(': ')
+            env["HTTP_#{k.upcase.tr('-', '_')}"] = v
           end
+
           status, headers, body = app.call(env)
+
           res_header = "HTTP/1.0 #{status} #{Rack::Utils::HTTP_STATUS_CODES[status.to_i]}\r\n"
           headers.each do |k, v|
             res_header << "#{k}: #{v}\r\n"
